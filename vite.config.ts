a9a174be3a45b008e2efd5fd130a5d6f7bb61801
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2026-05-28 16:55:34 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Movie-Hub/',
+
   server: {
     proxy: {
       '/api': {
         target: 'https://api.themoviedb.org/3',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/movies\?endpoint=/, '/'),
-      }
-    }
-  }
+        rewrite: (path) =>
+          path.replace(/^\/api\/movies\?endpoint=/, '/'),
+      },
+    },
+  },
 })
