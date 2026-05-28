4ed3cd447144370cd5dbb84cf8b10249ec2ca243
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2026-05-28 14:58:01 +0530

    commit

--- api/movie.js
+++ api/movie.js
@@ -1,19 +1,21 @@
-export default async function handler(req, res) {
-    const { endpoint } = req.query;
-  
-    if (!endpoint) {
-      return res.status(400).json({ error: "Missing endpoint" });
-    }
-  
-    const API_KEY = process.env.TMDB_API_KEY;
-    const separator = endpoint.includes("?") ? "&" : "?";
-    const url = `https://api.themoviedb.org/3${endpoint}${separator}api_key=${API_KEY}`;
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to fetch from TMDB" });
-    }
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = async function handler(req, res) {
+  const { endpoint } = req.query;
+
+  if (!endpoint) {
+    return res.status(400).json({ error: "Missing endpoint" });
   }
+
+  const API_KEY = process.env.TMDB_API_KEY;
+  const separator = endpoint.includes("?") ? "&" : "?";
+  const url = `https://api.themoviedb.org/3${endpoint}${separator}api_key=${API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch from TMDB" });
+  }
+}
